25efd39fe3acd84a4142c5fb196d8040a5d12957
Julian Zielke <13448042+jzielke84@users.noreply.github.com>
2024-01-01 15:57:34 +0100

    Split client count metric by connection type (#1)

--- metrics.go
+++ metrics.go
@@ -8,6 +8,13 @@ import (
 	"github.com/xperimental/freifunk-exporter/info"
 )
 
+const (
+	clientConnectionWifi24  = "wifi24"
+	clientConnectionWifi5   = "wifi5"
+	clientConnectionOther   = "other"
+	clientConnectionUnknown = "unknown"
+)
+
 var (
 	prefix = "freifunk_"
 
@@ -26,7 +33,7 @@ var (
 	clientCountDesc = prometheus.NewDesc(
 		prefix+"router_client_count_total",
 		"Number of connected clients",
-		[]string{"id"}, nil)
+		[]string{"id", "connection"}, nil)
 	loadAvgDesc = prometheus.NewDesc(
 		prefix+"router_load_avg_5m",
 		"Contains the five minutes average load for a router.",
@@ -108,9 +115,17 @@ func (c *collector) updateNodes(ch chan<- prometheus.Metric, nodes []info.Node) 
 		metaLabels := []string{node.ID, node.Hostname, node.Model, node.Firmware.Release, node.SiteCode}
 		sendMetric(ch, metaDesc, 1.0, metaLabels)
 
-		idLabel := []string{node.ID}
-		sendMetric(ch, clientCountDesc, float64(node.Clients), idLabel)
+		clientsUnknown := float64(node.Clients - node.ClientsWifi24 - node.ClientsWifi5 - node.ClientsOther)
+		if clientsUnknown < 0 {
+			clientsUnknown = 0
+		}
+		sendMetric(ch, clientCountDesc, clientsUnknown, []string{node.ID, clientConnectionUnknown})
 
+		sendMetric(ch, clientCountDesc, float64(node.ClientsWifi24), []string{node.ID, clientConnectionWifi24})
+		sendMetric(ch, clientCountDesc, float64(node.ClientsWifi5), []string{node.ID, clientConnectionWifi5})
+		sendMetric(ch, clientCountDesc, float64(node.ClientsOther), []string{node.ID, clientConnectionOther})
+
+		idLabel := []string{node.ID}
 		sendMetric(ch, loadAvgDesc, node.LoadAvg, idLabel)
 		sendMetric(ch, rootFsUsageDesc, node.RootfsUsage, idLabel)
 
